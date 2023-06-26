73d9473316dbfc1285fade426694b38e8f33593e
ml <miklakt@gmail.com>
2023-06-26 04:22:20 +0400

    new input files generator

--- task_gen_empty.py
+++ task_gen_empty.py
@@ -1,10 +1,15 @@
 #%%
+import pathlib
 import sfbox_utils
 import numpy as np
-#%%
-template =  sfbox_utils.read_input.parse_file("sfb_templates/template_empty.txt")
+import pandas as pd
+import itertools
+import utils
 
-def set_system_geometry(r, s, h, l1, l2):
+#%%
+ref_tbl = pd.read_pickle("reference_table_empty_brush.pkl")
+
+def set_system_geometry(r, s, h, l1, l2, **_):
     xlayers = r+h
     ylayers = l1+s+l2
 
@@ -22,7 +27,8 @@ def set_system_geometry(r, s, h, l1, l2):
 
     return d
 
-def set_polymer_brush(N, theta, r, s, l1):
+def set_polymer_brush(N, sigma, r, s, l1, **_):
+    theta_per_ring =  2*np.pi*r*N*sigma
     x1 = r+1
     y1 = l1+1
     y2 = l1+s
@@ -37,7 +43,7 @@ def set_polymer_brush(N, theta, r, s, l1):
         {f'mol:pol{i}:freedom': 'restricted' for i in range(s)}
     )
     d.update(
-        {f'mol:pol{i}:theta': theta for i in range(s)}
+        {f'mol:pol{i}:theta': theta_per_ring for i in range(s)}
     )
 
     d.update(
@@ -48,7 +54,7 @@ def set_polymer_brush(N, theta, r, s, l1):
     )
     return d
 
-def set_chi(chi_PW, chi_PS, s):
+def set_chi(chi_PW, chi_PS, s, **_):
 
     d={f'mon:P{i}:chi - W': chi_PW for i in range(s)}
 
@@ -56,56 +62,144 @@ def set_chi(chi_PW, chi_PS, s):
         {f'mon:P{i}:chi - S': chi_PS for i in range(s)}
     )
 
+    #d.update(
+    #    {f'mon:P{i}:chi - C': chi_PC for i in range(s)}
+    #)
+
     d.update(
         {
         f'mon:P:chi - W': chi_PW,
         f'mon:P:chi - S': chi_PS,
+        #f'mon:P:chi - C': chi_PC,
         }
     )
 
     return d
 
+def translation_chain(builder_funcs, **kwargs):
+    d ={}
+    for func in builder_funcs:
+        d.update(func(**kwargs))
+    return d
+
+def translate_to_sfbox(data):
+    init_keys = data[0]
+    
+    init_statements_builder = [
+        set_system_geometry,
+        set_polymer_brush,
+        set_chi,
+    ]
+    
+    followed_statements_builder = [
+        set_chi
+    ]
+
+    init_statement =  sfbox_utils.read_input.parse_file("sfb_templates/template_empty.txt")[0]
+    
+    init_statement.update(
+        translation_chain(init_statements_builder, **init_keys))
+
+    followed_statements = []
+
+    for followed_keys in data[1:]:
+        keys = dict(init_keys)
+        keys.update(followed_keys)
+        followed_statement = translation_chain(
+            followed_statements_builder, **keys
+            )
+        followed_statements.append(followed_statement)
+
+    return [init_statement] + followed_statements
+
+def sort_keys(list_of_dicts):
+    for i, d in enumerate(list_of_dicts):
+        list_of_dicts[i] = dict(sorted(d.items()))
+    return list_of_dicts
+
 def args_to_name(args, ignore_keys = []):
     return "_".join([str(k) + "=" + str(v) for k, v in args.items() if k not in ignore_keys])
 
-def generate_task_empty_pore(r, s, h, l1, l2, chi_PW, chi_PS, N, theta, **kwargs):
-    d ={}
+def continuously_change_chi_PS(
+        init_args,
+        chi_PS_start,
+        chi_PS_end,
+        chi_PS_step,
+        skip_found = True
+    ):
+    chi_PS_list = np.round(np.arange(chi_PS_start, chi_PS_end+chi_PS_step, chi_PS_step), 4)
 
-    d = set_system_geometry(r,s,h,l1,l2)
+    ifile_data = []
+    skipped = 0
 
-    d.update(set_polymer_brush(N, theta, r, s, l1))
+    for chi_PS in chi_PS_list:
+        record_in_reference = utils.get_by_kwargs(ref_tbl, chi_PS = chi_PS, **init_args)
 
-    d.update(set_chi(chi_PW, chi_PS, s))
+        if len(record_in_reference) == 0 or not(skip_found):
+            ifile_data.append(dict(chi_PS = chi_PS))
+        
+        else:
+            #print(f"The calculation is skipped {pc=}")
+            skipped = skipped+1
 
-    return d
+    if ifile_data:
+        ifile_data[0].update(init_args)
+
+    print(f"Calculation skipped: {skipped}")
+
+    return ifile_data
+
 # %%
-for chi in [1.1]:
-    args = dict(
-        r = 26,
-        s = 52,
-        h = 40,
-        l1 = 120,
-        l2 = 120,
-        chi_PS = chi,
-        chi_PW = 0,
-        N=300,
-        sigma = 0.02
-    )
-    args["theta"] =  2*np.pi*args["r"]*args["N"]*args["sigma"]
-    task = generate_task_empty_pore(**args)
+init_args = dict(
+    r = 26,
+    s = 52,
+    h = 40,
+    l1 = 120,
+    l2 = 120,
+    #chi_PS = chi,
+    chi_PW = 0,
+    N=300,
+    sigma = 0.02
+)
+working_dir = path = pathlib.Path("temp9")
+continue_unfinished = True
+try:
+    working_dir.mkdir(parents=True, exist_ok=False)
+except FileExistsError:
+    print("Folder is already there")
+#%%
+ifile_data = continuously_change_chi_PS(init_args, 1.1, 1.2, 0.01)
+translated = translate_to_sfbox(ifile_data)
+translated = sort_keys(translated)
 
-    template[0].update(task)
-
-    fname = args_to_name(args, ignore_keys=["theta"])+".in"
-    import pathlib
-    path = pathlib.Path("temp")
-    try:
-        path.mkdir(parents=True, exist_ok=False)
-    except FileExistsError:
-        print("Folder is already there")
+if continue_unfinished:
+    ig_data = utils.find_closest_in_reference(
+        ref_tbl,
+        init_args | {"chi_PS" : ("close", ifile_data[0]["chi_PS"])},
+        return_only_one = True
+        )
+    if (ig_data is not None) and (not ig_data.empty):
+        #print("Initial guess is found")
+        pass
     else:
-        print("Folder was created")
-    sfbox_utils.write_input.write_input_file(path/fname, template)
+        ig_data = False
 # %%
-sfbox_utils.sfbox_calls(parallel_execution='subprocess', dir = path)
+input_file = working_dir/(args_to_name(ifile_data[0], ignore_keys=["initial_guess"]) + ".in")
+initial_guess_file = input_file.with_suffix(".ig")
+# %%
+if ig_data is not False: 
+    utils.calculation_result_to_initial_guess_file(ig_data, initial_guess_file)
+# %%
+if initial_guess_file.exists():
+    print("Initial guess file is found")
+    translated[0].update({"newton:isaac:initial_guess":"file"})
+    translated[0].update({"newton:isaac:initial_guess_input_file":"filename.ig"})
+    try:
+        translated[1].update({"newton:isaac:initial_guess":"previous_result"})
+    except IndexError:
+        pass
+# %%
+sfbox_utils.write_input.write_input_file(input_file, translated)
+
+print(f"Number of calculations: {len(translated)}")
 # %%
