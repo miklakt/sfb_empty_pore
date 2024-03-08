681bd266e6c050f3c9335bb3a734c848c0a00bd2
ml <miklakt@gmail.com>
2024-03-08 02:58:05 +0100

    refactored to smaller funcs

--- calculate_fields_in_pore.py
+++ calculate_fields_in_pore.py
@@ -5,9 +5,12 @@ from scipy import ndimage
 from functools import lru_cache
 import utils
 from particle_convolution import convolve_particle_surface, convolve_particle_volume
-import pickle_cache
+#import pickle_cache
+from joblib import Memory
+import sfbox_utils
 np.seterr(divide='ignore')
 
+memory = Memory("__func_cache__", verbose=1)
 master_empty = pd.read_pickle("reference_table_empty_brush.pkl")
 
 def volume(d):
@@ -51,6 +54,9 @@ def mobility_Phillies(phi, beta, nu):
     m = np.where(phi==0, 1.0, np.exp(-beta*np.power(phi, nu)))
     return m
 
+def mobility_Holyst(phi, alpha, delta, N):
+    R_g = np.sqrt()
+
 @lru_cache()
 def generate_circle_kernel(d):
     radius = d/2
@@ -71,7 +77,7 @@ def add_walls(fields, exclude_volume = True, d = None):
     pore_radius = fields["r"]
     wall_thickness = fields["s"]
 
-    W_arr = np.zeros((xlayers, ylayers))
+    W_arr = np.zeros((ylayers,xlayers))
     W_arr[l1:l1+wall_thickness+1, pore_radius:] = True
     if exclude_volume:
         if d is None:
@@ -84,10 +90,10 @@ def calculate_pressure(fields, truncate = False):
     chi = fields["chi_PS"]
     fields["Pi"] = Pi(phi, chi, truncate)
 
-def calculate_gamma(fields, a0, a1):
+def calculate_gamma(fields, a0, a1, chi_PC):
     phi = fields["phi"]
     chi = fields["chi_PS"]
-    chi_PC = fields["chi_PC"]
+    #chi_PC = fields["chi_PC"]
     fields["gamma"] = gamma(chi, chi_PC, phi, a0, a1)
     fields["a0"] = a0
     fields["a1"] = a1
@@ -111,9 +117,9 @@ def calculate_energy(fields, d, method):
     #----total free energy----
     fields["free_energy"] = fields["surface"] + fields["osmotic"]
 
-def calculate_corrected_phi(fields, a0, a1, correction = "average", d = None, convolve_mode = "valid"):
+def calculate_corrected_phi(fields, a0, a1, chi_PC, correction = "average", d = None, convolve_mode = "valid"):
     phi = fields["phi"]
-    chi_PC = fields["chi_PC"]
+    #chi_PC = fields["chi_PC"]
     if correction != "none":
         if correction == "vol_average_corrected":
             average_corrected_phi = (a0 + a1*chi_PC)*phi
@@ -141,12 +147,32 @@ def calculate_mobility(fields, d, model, model_kwargs = {}, phi_arr = "phi"):
         xlayers = fields["xlayers"]
         ylayers = fields["ylayers"]
         mobility = np.ones((xlayers, ylayers))
-        mobility[fields["walls"]==True] = 0
     
+    mobility[fields["walls"]==True] = 0
     fields["mobility"] = mobility
 
+def calculate_conductivity(fields):
+    pore_radius = fields["r"]
+    r = np.arange(0, pore_radius)
+    
+    get_permeability_z = lambda x: np.sum(x[:, 0:pore_radius]*(2*r+1), axis = 1)#*2*np.pi
+    fields["conductivity"]  = fields["mobility"]*np.exp(-fields["free_energy"])
+    
+    fields["permeability_z"] = get_permeability_z(fields["conductivity"])
 
-@pickle_cache.pickle_lru_cache(purge_cache=True)
+def calculate_permeability(fields, L):
+    #====total permeability over region L====
+    l1 = fields["l1"].squeeze()
+    l2 = fields["l2"].squeeze()
+    wall_thickness = fields["s"].squeeze()
+    lb = l1-L
+    rb = l1+wall_thickness+L+1 #+1 because of the offset after convolution
+    get_permeability = lambda x: np.sum(x[lb:rb]**(-1))**(-1)
+    return get_permeability(fields["permeability_z"])
+
+
+#@pickle_cache.pickle_lru_cache(purge_cache=True)
+@memory.cache
 def calculate_fields(
         a0, a1, 
         chi_PC, chi, 
@@ -159,102 +185,78 @@ def calculate_fields(
         **mobility_model_kwargs
         ):
     fields = utils.get_by_kwargs(master_empty, chi_PS = chi, s = wall_thickness, r = pore_radius).squeeze()
-    phi = fields.dataset["phi"].squeeze().T
-    fields["phi"] = phi
-    l1 = fields["l1"].squeeze()
-    l2 = fields["l2"].squeeze()
-    
-    #==create walls==
-    W_arr = np.zeros_like(phi)
-    W_arr[l1:l1+wall_thickness+1, pore_radius:] = True
-    #----excluded volume----
-    if exclude_volume:
-        W_arr = ndimage.binary_dilation(W_arr, structure = generate_circle_kernel(d))
-    fields["walls"] = W_arr
+    fields["phi"] = fields.dataset["phi"].squeeze().T
 
-    #==calculate osmotic pressure and surface coef==
-    fields["Pi"] = Pi(phi, chi, truncate_pressure)
-    fields["gamma"] = gamma(chi, chi_PC, phi, a0, a1)
+    add_walls(fields, exclude_volume, d)
+    calculate_pressure(fields, truncate_pressure)
+    calculate_gamma(fields, a0, a1, chi_PC)
 
-    #==free energy calculation==
-    #----approximate method----
-    if method == "approx":
-        fields["surface"] = fields["gamma"]*surface(d)
-        fields["osmotic"] = fields["Pi"]*volume(d)
-    #----convolution method the output is staggered----
     if method == "convolve":
         convolve_mode = "valid"
-        fields["surface"] = convolve_particle_surface(fields["gamma"].T, d, convolve_mode).T
-        fields["osmotic"] = convolve_particle_volume(fields["Pi"].T, d, convolve_mode).T
-    #----convolution method the output is centered----
     if method == "convolve_same":
         convolve_mode = "same"
-        fields["surface"] = convolve_particle_surface(fields["gamma"].T, d, convolve_mode).T
-        fields["osmotic"] = convolve_particle_volume(fields["Pi"].T, d, convolve_mode).T
-    #----total free energy----
-    fields["free_energy"] = fields["surface"] + fields["osmotic"]
 
-    #==conductivity==
-    if mobility_correction == "no_mobility":
-        mobility = np.ones_like(fields["phi"])
-        mobility[fields["walls"]==True] = 0
-        fields["mobility"] = mobility
-    else:
-        if mobility_correction != "none":
-            if mobility_correction == "vol_average_corrected":
-                average_corrected_phi = (a0 + a1*chi_PC)*phi
-            elif mobility_correction == "vol_average":
-                average_corrected_phi = phi
-            fields["corrected_phi"] = convolve_particle_volume(average_corrected_phi.T, d, convolve_mode).T / volume(d)
-        else:
-            fields["corrected_phi"] = phi
-
-
-        if mobility_model=="Rubinstein":
-            if "prefactor" in mobility_model_kwargs:
-                prefactor = mobility_model_kwargs["prefactor"]
-            else:
-                prefactor = 1
-            k=1
-            mobility = mobility_Rubinstein(fields["corrected_phi"], k, d, prefactor)
-        
-        elif mobility_model=="Phillies":
-            beta = mobility_model_kwargs["beta"]
-            nu = mobility_model_kwargs["nu"]
-            mobility = mobility_Phillies(fields["corrected_phi"], beta, nu)
-
-    mobility[fields["walls"]==True] = 0
-    fields["mobility"] = mobility
-
-    #==pore permeability
-    #----pore with no polymer brush in it----
-    #for the reference and normalization
-    #empty_pore_conductivity = np.ones_like(fields["phi"])
-    #empty_pore_conductivity[fields["walls"]==True] = 0
-
-    #----permeability on z----
-    #numerical integration over z-crossection in cylindrical coordinates
-    r = np.arange(0, pore_radius)
-    get_permeability_z = lambda x: np.sum(x[:, 0:pore_radius]*(2*r+1), axis = 1)#*2*np.pi
-    fields["conductivity"]  = fields["mobility"]*np.exp(-fields["free_energy"])
-    
-    fields["permeability_z"] = get_permeability_z(fields["conductivity"])
-    #fields["permeability_z_empty"] = get_permeability_z(empty_pore_conductivity)
-
+    calculate_energy(fields, d, method)
+    calculate_corrected_phi(fields, a0, a1, mobility_correction, d, convolve_mode)
+    calculate_mobility(fields, d, mobility_model, mobility_model_kwargs)
+    calculate_conductivity(fields)
     return fields
 
 def integrate_permeability_over_z(fields, L):
-    #====total permeability over region L====
     l1 = fields["l1"].squeeze()
     l2 = fields["l2"].squeeze()
     wall_thickness = fields["s"].squeeze()
     lb = l1-L
     rb = l1+wall_thickness+L+1 #+1 because of the offset after convolution
     get_permeability = lambda x: np.sum(x[lb:rb]**(-1))**(-1)
-    #p = {}
-    #p["permeability"] = get_permeability(fields["permeability_z"])
-    #p["permeability_empty"] = get_permeability(fields["permeability_z_empty"])
-    #return p
     return get_permeability(fields["permeability_z"])
 
+def plot_heatmap(fields, r_cut, z_cut, keys, **kwargs):
+    from heatmap_explorer import plot_heatmap_and_profiles
+    wall_thickness = fields["s"]
+    l1 = fields["l1"]
+    def cut_and_mirror(arr):
+        cut = arr.T[0:r_cut, l1-z_cut:l1+wall_thickness+z_cut]
+        return np.vstack((np.flip(cut), cut[:,::-1]))
+    extent = [-z_cut-wall_thickness/2, z_cut+wall_thickness/2, -r_cut, r_cut]
+    for key in keys:
+        mask = cut_and_mirror(fields["walls"])
+        fig = plot_heatmap_and_profiles(
+            cut_and_mirror(fields[key]).T,
+            y0=-r_cut,
+            x0=-z_cut-wall_thickness/2,
+            ylabel="$r$",
+            xlabel = "$z$",
+            zlabel=key,
+            update_zlim=False,
+            hline_y=int(z_cut+wall_thickness/2),
+            vline_x=r_cut,
+            mask = mask.T,
+            **kwargs
+            )
+        fig.show()
+    return fig
+
 #%%
+if __name__ == "__main__":
+    parameters = dict(
+        a0 = 0.70585835,
+        a1 = -0.31406453, 
+        chi_PC = -1, 
+        chi = 0.5, 
+        wall_thickness=52, 
+        pore_radius=26, 
+        d = 8, 
+        exclude_volume = True, 
+        truncate_pressure = False, 
+        method = "convolve", 
+        mobility_correction = "vol_average",
+        mobility_model = "Rubinstein",
+    )
+
+    field = calculate_fields(**parameters)
+# %%
+r_cut = 50
+z_cut = 30
+plot_heatmap(field, r_cut, z_cut, keys = ["phi", "Pi", "gamma", "free_energy", "mobility", "conductivity"])
+
