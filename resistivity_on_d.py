ecc02b88dda6938dd7ae9508969fce80d27f30bf
ml <miklakt@gmail.com>
2024-05-23 01:50:05 +0200

    plot settings

--- resistivity_on_d.py
+++ resistivity_on_d.py
@@ -13,7 +13,6 @@ style.use('tableau-colorblind10')
 mpl_markers = ('o', '+', 'x', 's', 'D')
 
 from calculate_fields_in_pore import *
-from pickle_cache import pickle_lru_cache
 
 a0 = 0.70585835
 a1 = -0.31406453
@@ -41,6 +40,7 @@ simulation_results = pd.DataFrame(
         (18, 0.1, -2.0, 40.3837, 2.1726),
         (14, 0.1, -2.0, 58.1557, 0.5443),
         (4, 0.1, -2.0, 12.5677, 0.0287),
+        (6, 0.1, -2.0, 21.755, 0.0028),
         (10, 0.3, -1.75, 46.8653, 0.1363),
         (4, 0.1, -1.75, 9.0709, 0.0208),
         (6, 0.1, -1.75, 11.6507, 0.0269),
@@ -57,8 +57,6 @@ simulation_results = pd.DataFrame(
         (12, 0.3, -1.75, 53.1488, 0.8342),
         (14, 0.3, -1.75, 56.2245, 0.3548),
         (20, 0.3, -1.75, 56.7624, 1.5648),
-        # (24, 0.3, -1.75, 0.75, 100),
-        # (30, 0.3, -1.75, 0.1, 100)
     ]
 )
 
@@ -76,13 +74,14 @@ simulation_empty_pore = pd.DataFrame(
 d = np.arange(2, 24, 2)
 #d =[8 ,10, 12 ,]
 chi_PS = [0.1, 0.3, 0.5]
-chi_PC = [-2.5, -2.25, -2.0, -1.75, -1.5, -1.25, -1, -0.75]
+#chi_PC = [-2.5, -2.25, -2.0, -1.75, -1.5, -1.25, -1, -0.75]
 chi_PC_color = [-2.0, -1.75, -1.5, -1.25]
+chi_PC = chi_PC_color
 
 # model, mobility_model_kwargs = "none", {}
 # model, mobility_model_kwargs = "Phillies", dict(beta = 8, nu = 0.76)
 # model = "Fox-Flory", dict(N = 300)
-model, mobility_model_kwargs = "Rubinstein", {"prefactor":1}
+model, mobility_model_kwargs = "Rubinstein", {"prefactor":100}
 #model, mobility_model_kwargs = "Hoyst", {"alpha" : 1.63, "delta": 0.89, "N" : 300}
 
 results = []
@@ -107,15 +106,26 @@ for d_, chi_PS_, chi_PC_ in itertools.product(d, chi_PS, chi_PC):
 results = pd.DataFrame(results)
 
 #%%
-fig, axs = plt.subplots(ncols = len(chi_PS), sharey="row", nrows = 3, sharex = True)
+show_contributions = True
+show_CFD = False
+show_analytical = True
+if show_contributions:
+    fig, axs = plt.subplots(ncols = len(chi_PS), sharey="row", nrows = 3, sharex = True)
+    first_row_axes = axs[0, :]
+else:
+    fig, axs = plt.subplots(ncols = len(chi_PS), sharey="row", nrows = 1, sharex = True)
+    first_row_axes = axs
 results_ = results.loc[(results.mobility_model == model)]
 
-
-for ax, (chi_PS_, result_) in zip(axs[0, :], results_.groupby(by = "chi")):
+for ax, (chi_PS_, result_) in zip(first_row_axes, results_.groupby(by = "chi")):
     markers = itertools.cycle(mpl_markers)
     for chi_PC_, result__ in result_.groupby(by = "chi_PC"):
         x = result__["d"].squeeze()
         y = 1/result__["permeability"]
+
+        # y = np.gradient(np.log(y))/np.gradient(np.log(x))
+        # y = np.gradient(y)/np.gradient(np.log(x))
+
         if chi_PC_ in chi_PC_color:
             plot_kwargs = dict(
                 label = fr"$\chi_{{PC}} = {chi_PC_}$",
@@ -133,119 +143,151 @@ for ax, (chi_PS_, result_) in zip(axs[0, :], results_.groupby(by = "chi")):
             **plot_kwargs
             )
 
-        R = simulation_results.query(f"(chi_PS=={chi_PS_})&(chi_PC=={chi_PC_})")
+        if show_CFD:
+            R = simulation_results.query(f"(chi_PS=={chi_PS_})&(chi_PC=={chi_PC_})")
+            ax.scatter(
+                R["d"], 1/(R["J_tot"]/R["d"]/3), 
+                color = ax.lines[-1].get_color(),
+                marker = "s",
+                facecolor = "none",
+                s=20,
+                linewidth = 0.5
+                )
+
+    if show_analytical:
+        ax.plot(
+            d, 
+            1/result__["thick_empty_pore"], 
+            color = "black", 
+            linestyle = "-",
+            label = "$P_{empty}$",
+            linewidth = 2,
+            zorder = -1,
+            )
+
+        ax.plot(
+            d, 
+            1/result__["thin_empty_pore"], 
+            color = "black", 
+            linestyle = ":",
+            label = "$P_{thin}$",
+            zorder = -1,
+            )
+
+        R_cylinder = wall_thickness/(np.pi*(pore_radius-d/2)**2)/result__["einstein_factor"]
+        ax.plot(
+            d, 
+            R_cylinder, 
+            color = "black", 
+            linestyle = "-.",
+            label = "$P_{thin}$",
+            zorder = -1,
+            )
+
+    if show_CFD:
         ax.scatter(
-            R["d"], 1/(R["J_tot"]/R["d"]/3), 
-            color = ax.lines[-1].get_color(),
-            marker = "o",
-            facecolor = "none",
-            s=30,
+            simulation_empty_pore["d"], 
+            1/(simulation_empty_pore["J_tot"]/simulation_empty_pore["d"]/3),
+            color = "black",
+            #facecolor = "none",
+            marker = "s",
+            label = "numerical\n simulation",
+            s=20,
             linewidth = 0.5
             )
 
-    ax.plot(
-        d, 
-        1/result__["thick_empty_pore"], 
-        color = "black", 
-        linestyle = "--",
-        label = "$P_{empty}$"
-        )
-
-    ax.plot(
-        d, 
-        1/result__["thin_empty_pore"], 
-        color = "black", 
-        linestyle = ":",
-        label = "$P_{thin}$"
-        )
-
-    ax.scatter(
-        simulation_empty_pore["d"], 
-        1/(simulation_empty_pore["J_tot"]/simulation_empty_pore["d"]/3),
-        color = "black",
-        facecolor = "none",
-        marker = "o",
-        label = "numerical\n simulation",
-        s=30,
-        linewidth = 0.5
-        )
-
     ax.set_title(f"$\chi_{{PS}} = {chi_PS_}$")
     ax.set_ylim(1e-1, 1e3)
+    #ax.set_ylim(-5, 20)
     ax.set_yscale("log")
     ax.set_xscale("log")
 
-for ax, (chi_PS_, result_) in zip(axs[1,:], results_.groupby(by = "chi")):
-    markers = itertools.cycle(mpl_markers)
-    for chi_PC_, result__ in result_.groupby(by = "chi_PC"):
-        x = result__["d"].squeeze()
-        R0 = 1/result__["thin_empty_pore"]
-        y = (result__["R_left"] + result__["R_right"])/R0
-        if chi_PC_ in chi_PC_color:
-            plot_kwargs = dict(
-                label = fr"$\chi_{{PC}} = {chi_PC_}$",
-                #marker = next(markers),
-                #markevery = 0.5,
-                #markersize = 4,
-            )
-        else:
-            plot_kwargs = dict(
-                linewidth = 0.1,
-                color ="black"
-            )
-        ax.plot(
-            x, y, 
-            **plot_kwargs
-            )
-    
-    ax.axhline(1, color = "red", linestyle = "-", linewidth = 0.5)
+if show_contributions:
+    for ax, (chi_PS_, result_) in zip(axs[1,:], results_.groupby(by = "chi")):
+        markers = itertools.cycle(mpl_markers)
+        for chi_PC_, result__ in result_.groupby(by = "chi_PC"):
+            x = result__["d"].squeeze()
+            R0 = 1/result__["thin_empty_pore"]
+            y = (result__["R_left"] + result__["R_right"])/R0
+            if chi_PC_ in chi_PC_color:
+                plot_kwargs = dict(
+                    label = fr"$\chi_{{PC}} = {chi_PC_}$",
+                    #marker = next(markers),
+                    #markevery = 0.5,
+                    #markersize = 4,
+                )
+            else:
+                plot_kwargs = dict(
+                    linewidth = 0.1,
+                    color ="black"
+                )
+            ax.plot(
+                x, y, 
+                **plot_kwargs
+                )
+        
+        ax.axhline(1, color = "black", linestyle = ":", linewidth = 2, zorder = -1)
 
-    #ax.set_title(f"$\chi_{{PS}} = {chi_PS_}$")
-    ax.set_ylim(5e-2, 1e3)
-    #ax.set_xlabel("d")
-    ax.set_yscale("log")
-    ax.set_xscale("log")
+        #ax.set_title(f"$\chi_{{PS}} = {chi_PS_}$")
+        ax.set_ylim(5e-2, 1e3)
+        #ax.set_xlabel("d")
+        ax.set_yscale("log")
+        ax.set_xscale("log")
 
-for ax, (chi_PS_, result_) in zip(axs[2,:], results_.groupby(by = "chi")):
-    markers = itertools.cycle(mpl_markers)
-    for chi_PC_, result__ in result_.groupby(by = "chi_PC"):
-        x = result__["d"].squeeze()
-        R0 = wall_thickness/(np.pi*(pore_radius-d/2)**2)/result__["einstein_factor"]
-        y = (result__["R_pore"])/R0
-        if chi_PC_ in chi_PC_color:
-            plot_kwargs = dict(
-                label = fr"$\chi_{{PC}} = {chi_PC_}$",
-                #marker = next(markers),
-                #markevery = 0.5,
-                #markersize = 4,
-            )
-        else:
-            plot_kwargs = dict(
-                linewidth = 0.1,
-                color ="black"
-            )
-        ax.plot(
-            x, y, 
-            **plot_kwargs
-            )
+    for ax, (chi_PS_, result_) in zip(axs[2,:], results_.groupby(by = "chi")):
+        markers = itertools.cycle(mpl_markers)
+        for chi_PC_, result__ in result_.groupby(by = "chi_PC"):
+            x = result__["d"].squeeze()
+            R0 = wall_thickness/(np.pi*(pore_radius-d/2)**2)/result__["einstein_factor"]
+            y = (result__["R_pore"])/R0
+            if chi_PC_ in chi_PC_color:
+                plot_kwargs = dict(
+                    label = fr"$\chi_{{PC}} = {chi_PC_}$",
+                    #marker = next(markers),
+                    #markevery = 0.5,
+                    #markersize = 4,
+                )
+            else:
+                plot_kwargs = dict(
+                    linewidth = 0.1,
+                    color ="black"
+                )
+            ax.plot(
+                x, y, 
+                **plot_kwargs
+                )
 
-    ax.axhline(1, color = "red", linestyle = "-", linewidth = 0.5)
+        ax.axhline(1, color = "black", linestyle = "-.", linewidth = 2, zorder = -1)
 
-    ax.set_ylim(1e-3, 1e3)
-    ax.set_xlabel("d")
-    ax.set_yscale("log")
-    ax.set_xscale("log")
+        ax.set_ylim(1e-3, 1e3)
+        ax.set_xlabel("d")
+        ax.set_yscale("log")
+        ax.set_xscale("log")
 
-axs[0,0].set_ylabel(r"$R \cdot \frac{k_B T}{\eta_0}$")
-axs[1,0].set_ylabel(r"$R_{conv}/R_{thin}(d)$")
-axs[2,0].set_ylabel(r"$\frac{R_{pore}}{\pi(r-d/2)^2} \frac{k_B T}{3 \pi \eta_0 d}$")
-# axs[1,-1].legend( 
-#     bbox_to_anchor = [1.0, -0.05],
-#     loc = "lower left"
-#     )
-axs[1,-1].legend()
+    axs[0,0].set_ylabel(r"$R \cdot \frac{k_B T}{\eta_0}$")
+    axs[1,0].set_ylabel(r"$R_{conv}/R_{thin}(d)$")
+    axs[2,0].set_ylabel(r"$\frac{R_{cylinder}}{\pi(r-d/2)^2} \frac{k_B T}{3 \pi \eta_0 d}$")
+    # axs[1,-1].legend( 
+    #     bbox_to_anchor = [1.0, -0.05],
+    #     loc = "lower left"
+    #     )
+    axs[1,-1].plot([],[], color = "black", linestyle = ":", linewidth = 2,
+
+                    label = "$R_{conv}$"
+                    )
+
+    axs[1,-1].plot([],[], color = "black", linestyle = "-.", linewidth = 2,
+                    label = "$R_{channel}$"
+                    )
+
+    axs[1,-1].plot([],[], color = "black", linestyle = "-", linewidth = 2,
+                    label = "$R_{empty}$"
+                    )
+
+    #axs[1,-1].legend()
 
 plt.tight_layout()
 fig.set_size_inches(7, 7)
 #fig.savefig("fig/permeability_on_d.svg")
+#fig.savefig("tex/third_report/fig/permeability_on_d_detailed_low_d.svg")
 # %%
