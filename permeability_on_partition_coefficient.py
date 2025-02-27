4c124c7f0bf39530df8aaad4ba7820c6363171bf
ml <miklakt@gmail.com>
2025-02-27 22:50:35 +0100

    experimental data

--- permeability_on_partition_coefficient.py
+++ permeability_on_partition_coefficient.py
@@ -27,6 +27,8 @@ def free_energy_gel(phi, chi_PS, chi_PC, d):
     Pi_ = Pi(phi, chi_PS, trunc=False)
     gamma_ = gamma(chi_PS, chi_PC, phi, a0, a1)
     free_energy = Pi_*V + gamma_*S
+    #print(Pi_)
+    # free_energy = gamma_*S
     return free_energy
 
 def PC_gel(phi, chi_PS, chi_PC, d):
@@ -35,9 +37,10 @@ def PC_gel(phi, chi_PS, chi_PC, d):
     return PC
 #%%
 #d = np.arange(6, 24, 2)
+#d = [4,6]
 d = [6]
-chi_PS = [0.5, 0.6]
-chi_PC = np.round(np.arange(-3, 0.2, 0.05),3)
+chi_PS = [0.6]
+chi_PC = np.round(np.arange(-5, 0.2, 0.05),3)
 
 # model, mobility_model_kwargs = "none", {}
 # model, mobility_model_kwargs = "Phillies", dict(beta = 8, nu = 0.76)
@@ -68,8 +71,8 @@ for d_, chi_PS_, chi_PC_ in itertools.product(d, chi_PS, chi_PC):
     
     results.append(result)
 results = pd.DataFrame(results)
+results["permeability_channel"] = results["R_pore"]**(-1)
 #%%
-experimental_data = pd.read_csv("perm_rates_experimental_data.csv")
 #%%
 def create_interp_func(X, Y, domain=None):
     from scipy.interpolate import CubicSpline
@@ -109,12 +112,23 @@ def chi_from_phi_min_spinodal(N, phi_min):
     """
     return 0.5 * (1.0/(N * phi_min) + 1.0/(1.0 - phi_min))
 
+def chi_from_phi_binodal(phi):
+    return (-np.log(1-phi) - phi)/(phi**2)
+
 def estimate_protein_diameter(MW_kDa, density=1.35):
+
     NA = 6.022e23
-    MW_g_per_mol = MW_kDa * 1000.0
-    mass_per_molecule_g = MW_g_per_mol / NA
-    volume_cm3 = mass_per_molecule_g / density
-    diameter_nm = ((3.0 * volume_cm3) / (2.0 * np.pi)) ** (1.0 / 3.0) * 1e7
+
+    # Partial specific volume (cm^3/g)
+    v_bar = 1/density
+
+    mw_g_per_mol = MW_kDa * 1000.0
+    mass_one_molecule = mw_g_per_mol / NA
+    volume_cm3 = mass_one_molecule * v_bar
+    volume_nm3 = volume_cm3 * 1.0e21
+    radius_nm = ((3.0 * volume_nm3) / (4.0 * np.pi)) ** (1.0 / 3.0)
+    diameter_nm = 2.0 * radius_nm
+
     return diameter_nm
 
 def protein_mass_fraction(
@@ -129,67 +143,35 @@ def protein_mass_fraction(
     mass_water_g = volume_water_ml * water_density
     mass_total_g = mass_protein_g + mass_water_g
     return mass_protein_g / mass_total_g if mass_total_g > 0 else float('nan')
-
+#%%
+experimental_data = pd.read_csv("perm_rates_experimental_data.csv")
+globular_protein_density = 1.35#g/ml
+Kuhn_segment = 0.76
+experimental_data["d"] = experimental_data.apply(lambda _:estimate_protein_diameter(_.MW, globular_protein_density), axis = 1)
+experimental_data["d_a"] = experimental_data["d"]/Kuhn_segment
 #%%
 reference_chi_PC = 0.0
-#reference_d = 6
-Kuhn_segment = 0.76
-#phi_gel = 0.2
-#chi_gel = 0.65
-
+reference_d = 6
 protein_density = 1.25#g/ml
-phi_gel = {
-    "Mac98A":protein_mass_fraction(
-                conc_mg_per_ml=175, 
-                protein_density=protein_density
-                ),
-    "Nup116":protein_mass_fraction(
-                conc_mg_per_ml=200, 
-                protein_density=protein_density
-                ),
-    "Nsp1": protein_mass_fraction(
-                conc_mg_per_ml=200, 
-                protein_density=protein_density
-                )
-}
-
-# phi_gel = protein_mass_fraction(
-#     conc_mg_per_ml=250, 
-#     protein_density=1.3
-#     ) #0.24
-
-chi_gel = {key:chi_from_phi_min_spinodal(N=300, phi_min=phi_gel[key]) for key in phi_gel}
-# chi_gel = chi_from_phi_min_spinodal(
-#     N=300, phi_min=phi_gel
-#     )#0.664
 
 #%%
-phi_gel=0.2
-chi_gel=0.65
-results["PC_gel"]=results.apply(lambda _:PC_gel(phi_gel, chi_gel, _["chi_PC"], _["d"]), axis = 1)
-
-def get_permeability_different_bc(boundary_z, permeability):
-    R_reservoir = (-np.log(pore_radius/3 + boundary_z) + np.log(pore_radius + boundary_z))/(2*np.pi*pore_radius)
-    return (permeability**(-1)-R_reservoir)**(-1)
-
-def get_permeability_longer_pore(add_length, permeability_z, permeability):
-    R_added  = (permeability_z[int(len(permeability_z)/2)])**(-1)*add_length
-    return (permeability**(-1)+R_added)**(-1)
-
-
-# add_length=150
-# results["permeability_longer"]=results.apply(lambda _:get_permeability_longer_pore(add_length, _.permeability_z, _.permeability), axis = 1)
-
-results["permeability_channel"] = results["R_pore"]**(-1)
-
-globular_protein_density = 1.25#g/ml
-experimental_data["d"] = experimental_data.apply(lambda _:estimate_protein_diameter(_.MW, globular_protein_density), axis = 1)
-# boundary_z = 0
-# results["permeability_bc"]=results.apply(lambda _:get_permeability_different_bc(boundary_z, _.permeability), axis = 1)
-r_pore=r_dome = 26
+#einstein_factor = 1/(3*np.pi*d[0])
+#r_pore=26
+#r_dome = r_pore*2
 wall_thickness = 26
-dome_permeability = 4*np.pi*r_dome
-thin_pore_permeabiity = empty_pore_permeability(1,r_pore,wall_thickness=26)
+#dome_permeability = np.pi*r_dome*einstein_factor
+#thin_pore_permeability = empty_pore_permeability(1,r_pore-d[0]/2,0)*einstein_factor
+#thick_pore_permeability = empty_pore_permeability(1,r_pore,wall_thickness)*einstein_factor
+
+def get_PC_in_gel(phi_gel, chi_PC, d):
+    chi_gel=chi_from_phi_binodal(phi_gel)
+    return PC_gel(phi_gel, chi_gel, chi_PC, d)
+
+def get_thin_pore_permeability(d, r_pore = 26):
+    return empty_pore_permeability(1.0,r_pore-d/2, 0)/(3*np.pi*d)
+
+def get_dome_permeability(d, r_dome = 26*1.5):
+    return np.pi*r_dome/(3*np.pi*d)
 #%%
 nups = ["Mac98A","Nup116"]
 experimental_data_=experimental_data.loc[experimental_data.Oligomer == 1]
@@ -201,7 +183,7 @@ if len(chi_PS) == 1:
     axs_ = [axs]
 else:
     axs_ = axs
-results_ = results.loc[(results.mobility_model == model)]
+results_ = results.loc[(results.mobility_model == model)].sort_values(by = "chi_PC")
 
 for ax, (chi_PS_, result_) in zip(axs_, results_.groupby(by = "chi")):
     y = experimental_data_["Passage_Rate"]
@@ -228,39 +210,80 @@ for ax, (chi_PS_, result_) in zip(axs_, results_.groupby(by = "chi")):
             label = None
         ax.scatter(x,y, color = color, marker = next(markers), s = s*3, label=label)
     
-    markers = itertools.cycle(mpl_markers)
-    first_it =True
-    for nup in nups:
-        mark_protein = "EGFP"
-        color = "green"
-        y = experimental_data_.loc[experimental_data_.Protein == mark_protein,"Passage_Rate"]
-        x = experimental_data_.loc[experimental_data_.Protein == mark_protein,nup]
-        s = experimental_data_.loc[experimental_data_.Protein == mark_protein,"d"]
-        if first_it: 
-            label = mark_protein
-            first_it = False
-        else:
-            label = None
-        ax.scatter(x,y, color = color, marker = next(markers), s = s*3, label=label)
+    # markers = itertools.cycle(mpl_markers)
+    # first_it =True
+    # for nup in nups:
+    #     mark_protein = "EGFP"
+    #     color = "green"
+    #     y = experimental_data_.loc[experimental_data_.Protein == mark_protein,"Passage_Rate"]
+    #     x = experimental_data_.loc[experimental_data_.Protein == mark_protein,nup]
+    #     s = experimental_data_.loc[experimental_data_.Protein == mark_protein,"d"]
+    #     if first_it: 
+    #         label = mark_protein
+    #         first_it = False
+    #     else:
+    #         label = None
+    #     ax.scatter(x,y, color = color, marker = next(markers), s = s*3, label=label)
 
     for d_, result__ in result_.groupby(by = "d"):
-        permeability_key = "permeability_channel"
+        permeability_key = "permeability"
         #reference_permeability = result_.loc[(result_.chi_PC==reference_chi_PC) & (result__.d==reference_d),permeability_key].squeeze()
         reference_permeability = result__.loc[(result__.chi_PC==reference_chi_PC),permeability_key].squeeze()
         y = result__[permeability_key].squeeze()#*d_
-        x = result__["PC_gel"].squeeze()
         y = y/reference_permeability
+        #x = result__["PC_gel"].squeeze()
+        phi_gel_min = 0.2
+        phi_gel_max = 0.4
+        phi_gel_mean = (phi_gel_max+phi_gel_min)/2
+        x = get_PC_in_gel(phi_gel_mean, result__["chi_PC"], d_)
         ax.plot(
             x, y, 
             label = fr"$d = {d_}({d_*Kuhn_segment:.1f}"+r"\text{nm})$",
+            alpha = 1,
             #marker = next(markers),
             #markevery = 0.5,
             #markersize = 4,
         )
-        normalized_dome_permeability = dome_permeability/reference_permeability
-        ax.axhline(normalized_dome_permeability)
+        color_ = ax.lines[-1].get_color()
+
+        for phi_gel_ in np.linspace(phi_gel_min, phi_gel_max,50):
+            x = get_PC_in_gel(phi_gel_, result__["chi_PC"], d_)
+            ax.plot(
+                x, y, 
+                alpha = 0.1,
+                color = color_
+            )
+        # for phi_gel_ in np.linspace(phi_gel_min, phi_gel_max,50):
+        #     chi_gel_eq = chi_from_phi_binodal(phi_gel_)
+        #     for chi_gel_ in np.linspace(chi_gel_eq-0.05, chi_gel_eq+0.05, 20):
+        #         x = PC_gel(phi_gel_,chi_gel_, result__["chi_PC"], d_)
+        #         ax.plot(
+        #             x, y, 
+        #             alpha = 0.1,
+        #             color = color_
+        #         )
+        
+        
+        normalized_thin_pore_permeability = get_thin_pore_permeability(d_)/reference_permeability
+        ax.axhline(normalized_thin_pore_permeability, color =  ax.lines[-1].get_color(), linestyle = "--", label = "thin pore")
+
+        normalized_dome_permeability = get_dome_permeability(d_)/reference_permeability
+        ax.axhline(normalized_dome_permeability, color =  ax.lines[-1].get_color(), linestyle = "--", linewidth = 0.3, label = "conductive sphere")
+
+
+        ax.plot(x.iloc[::10], [10**5]*len(x.to_numpy()[::10]), marker = "|", linewidth = 0, color = "black")
+        
+        for xx, ss in zip(x.iloc[::10].squeeze(), result__["chi_PC"].iloc[::10].squeeze()):
+            if ss<=-3.0: continue
+            ax.text(xx*0.4, 10**5*1.1, s = f"{ss:.1f}")
+
+        # x = result__["chi_PC"].squeeze()
+        # ax2 = ax.twiny()
+        # ax2.plot(x,y)
+        # ax2.set_ylim(ax2.get_ylim()[::-1])
    
-        # permeability_key = "permeability"
+        #permeability_key = "permeability_channel"
+
         # reference_permeability = result__.loc[result__.chi_PC==reference_chi_PC,permeability_key].squeeze()
         # y = result__[permeability_key].squeeze()#*d_
         # #y = result__["PC"].squeeze()
@@ -293,9 +316,9 @@ for ax, (chi_PS_, result_) in zip(axs_, results_.groupby(by = "chi")):
     #             fontsize=5
     #         )
 
-    ax.set_title(r"$\chi_{{PS}} = "+f"{chi_PS_}$")
-    ax.set_ylim(1e-1, 1e5)
-    ax.set_xlim(1e-2,1e4)
+    ax.set_title(r"$\chi_{\text{PC}}$", pad = 18)
+    ax.set_ylim(1e-1, 1e5 )
+    ax.set_xlim(1e-2,1e8)
     ax.set_xlabel(r"$c_{\text{eq}}/c_0$")
     ax.set_yscale("log")
     ax.set_xscale("log")
@@ -312,6 +335,9 @@ axs_[-1].legend(
     bbox_to_anchor = [1.05, 0.05],
     loc = "lower left"
     )
+
+fig.text(1,0.8, r"$\chi_{\text{PS}}^{\text{pore}} = "+f"{chi_PS_}$")
+fig.text(1,0.7, r"$\chi_{\text{PS}}^{\text{gel}} = \{\Pi(\phi_{\text{gel}}) = 0\}$")
 
 #plt.tight_layout()
 fig.set_size_inches(2.5*len(axs_)+0.5, 3)
