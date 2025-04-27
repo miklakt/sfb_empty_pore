f69d24af07253a275ff35c9447956f4dfce36de1
ml <miklakt@gmail.com>
2025-04-27 16:18:53 +0200

    cleanup

--- plot_rate_referenced.py
+++ plot_rate_referenced.py
@@ -50,7 +50,7 @@ simulation_empty_pore["R_corrected"] = 1/(simulation_empty_pore["J_corrected"]/s
 #%%
 d = np.arange(2, 20, 2)
 #d =[8 ,10, 12 ,]
-chi_PS = [0.6]
+chi_PS = [0.4, 0.5, 0.6]
 #chi_PC = [-2.5, -2.25, -2.0, -1.75, -1.5, -1.25, -1, -0.75]
 chi_PC_color = [0, -0.5]
 chi_PC = chi_PC_color
@@ -230,7 +230,7 @@ def get_k_empty_pore(
     k_B = 1.380649*1e-23   #J/K
 
     r_p_ = (r_p-d/2)*1e-9            #m^3
-    L_ = (L+d)*1e-9                #m^3
+    L_ = (L+d)*1e-9                  #m^3
     d_ = d*1e-9                      #m^3
     eta_ = eta                       #Pa*s
     V_nucleus_ = V_nucleus*1e-18     #m^3
@@ -245,127 +245,22 @@ def get_k_empty_pore(
     k_ = NPC_per_nucleus/V_nucleus_/R_0_       #1/s
     
     return k_
-#%%
-# fig, axs = plt.subplots(ncols = len(chi_PS), sharey="row", nrows = 1, sharex = True)
-# if len(chi_PS) == 1:
-#     axs_ = [axs]
-# else:
-#     axs_ = axs
 
-# #reference = results.loc[(results.d==6)]
-
-
-
-# for ax, (chi_PS_, result_) in zip(axs_, results.groupby(by = "chi")):
-#     markers = itertools.cycle(mpl_markers)
-#     for chi_PC_, result__ in result_.groupby(by = "chi_PC"):
-        
-#         #reference_ = reference.loc[(reference.chi == chi_PS_)&(reference.chi_PC == chi_PC_), "permeability"].squeeze()
-
-#         x = result__["d"].squeeze()*Kuhn_segment
-#         y = result__["permeability"]
-
-#         interp_func = create_interp_func(x,np.log(y))
-
-#         reference_permeability = np.exp(interp_func(reference_particle_radius*2))
-#         y = result__["permeability"]/reference_permeability
-
-#         if chi_PC_ in chi_PC_color:
-#             plot_kwargs = dict(
-#                 label = fr"$\chi_{{\text{{PC}}}} = {chi_PC_}$",
-#                 #label = fr"${chi_PC_:.2f}$",
-#                 #marker = next(markers),
-#                 #markevery = 0.5,
-#                 #markersize = 4,
-#             )
-#         else:
-#             plot_kwargs = dict(
-#                 linewidth = 0.1,
-#                 color ="black"
-#             )
-
-#         ax.plot(
-#             x, y, 
-#             **plot_kwargs,
-#             marker = next(markers),
-#             #mfc = "none",
-#             ms = 3,
-#             linewidth = 0.5
-#             )
-
-#         ax.scatter(
-#             experimental_data["d"], 
-#             experimental_data["qi"], 
-#             color = "black", 
-#             linewidth = 0.1, 
-#             marker = "*", 
-#             )
-
-#     ax.scatter(
-#         [], 
-#         [], 
-#         color = "black", 
-#         linewidth = 0.1, 
-#         marker = "*", 
-#         label ="experimental"
-#     )
-
-#     reference_probe = "Thioredoxin"
-#     for idx, row in experimental_data.iterrows():
-#         if row["Probe"] == reference_probe:
-#             color = "Red"
-#         else:
-#             color = None
-#         ax.text(
-#             x = row["d"],
-#             y = row["qi"],
-#             s = row["Probe"],
-#             rotation = 90,
-#             va = "top",
-#             ha = "center",
-#             color = color
-#         )
-    
-#     ref_record = experimental_data.loc[experimental_data["Probe"] == reference_probe]
-#     x = ref_record["d"].squeeze()
-#     y = ref_record["qi"].squeeze()
-#     ax.scatter(
-#         [x], 
-#         [y], 
-#         color = "red", 
-#         linewidth = 0.1, 
-#         marker = "*", 
-#         #label =reference_probe
-#     )
-
-#     reference_probe = "GFP"
-#     ref_record = experimental_data.loc[experimental_data["Probe"] == reference_probe]
-#     x = ref_record["d"].squeeze()
-#     y = ref_record["qi"].squeeze()
-#     ax.scatter(
-#         [x], 
-#         [y], 
-#         color = "green", 
-#         linewidth = 0.1, 
-#         marker = "*", 
-#         #label =reference_probe
-#     )
-
-
-#     ax.set_yscale("log")
-#     ax.set_title(r"$\chi_{\text{PS}}="+f"{chi_PS_}$")
-#     ax.set_xlabel("d, nm")
-#     ax.set_ylim(1e-2,1e2)
-#     ax.set_xlim(1.2,6.2)
-
-# axs_[0].set_ylabel(rf"$R(d = {reference_particle_radius*2}"+r"\text{nm}) / R$")
-# ax.legend(
-#     #title = r"$\chi_{\text{PC}}$"
-#     )
-# #fig.set_size_inches(2.5*len(chi_PS)+1, 3)
-# plt.tight_layout()
-# fig.set_size_inches(2.5, 2.5)
-# #fig.savefig("fig/experimental_inert.svg")
+def expression(d):
+    numerator = (
+        7.848e5 * d**3
+        - 9.304e7 * d**2
+        + 3.677e9 * d
+        - 4.844e10
+    )
+    denominator = (
+        d * (
+            2.793e5 * d**2
+            + 8.079e7 * d
+            - 3.629e9
+        )
+    )
+    return numerator / denominator
 # %%
 fig, axs = plt.subplots(ncols = len(chi_PS), sharey="row", nrows = 1, sharex = True)
 if len(chi_PS) == 1:
@@ -415,26 +310,19 @@ for ax, (chi_PS_, result_) in zip(axs_, results.groupby(by = "chi")):
             color = "black"
             )
         
-        y2 = [get_k_empty_pore(
-                    pore_radius*Kuhn_segment,
-                    wall_thickness*Kuhn_segment,
-                    x2_,
-                    NPC_per_nucleus,
-                    nucleus_volume,
-                    eta=eta,
-                    Haberman_correction=True
-                    ) for x2_ in x2]
+        # y2 = [expression(x2_,
+        #             ) for x2_ in x2]
         
-        ax.plot(
-            x2, y2, 
-            label = "empty",
-            #marker = next(markers),
-            #mfc = "none",
-            #ms = 3,
-            linewidth = 0.5,
-            linestyle = "--",
-            color = "black"
-            )
+        # ax.plot(
+        #     x2, y2, 
+        #     label = "empty",
+        #     #marker = next(markers),
+        #     #mfc = "none",
+        #     #ms = 3,
+        #     linewidth = 0.5,
+        #     linestyle = "--",
+        #     color = "red"
+        #     )
         
 
         if chi_PC_ in chi_PC_color:
@@ -545,6 +433,6 @@ axs_[0].set_ylabel(r"$k \, [s^{-1}]$")
 #     )
 #fig.set_size_inches(2.5*len(chi_PS)+1, 3)
 plt.tight_layout()
-fig.set_size_inches(2.5, 2.5)
+fig.set_size_inches(2.5*3, 2.5)
 #fig.savefig("fig/experimental_inert.svg")
 # %%
