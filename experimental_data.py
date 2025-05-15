7e61c6e60cefde7edb208e0e9b68ef2e07bcfb16
ml <miklakt@gmail.com>
2025-05-15 02:13:18 +0200

    added attr particle data

--- experimental_data.py
+++ experimental_data.py
@@ -1,4 +1,5 @@
 #%%
+from io import StringIO
 import itertools
 import numpy as np
 import pandas as pd
@@ -10,11 +11,9 @@ from matplotlib import rc
 #rc('text',usetex=True)
 #rc('text.latex', preamble=r'\usepackage{color}')
 style.use('tableau-colorblind10')
-mpl_markers = ('o', '+', 'x', 's', 'D')
 
 #from calculate_fields_in_pore import *
 
-flux_vs_molar_weight = {}
 #%%
 def k_from_normalized_R(
         R_normalized,
@@ -187,6 +186,7 @@ def R_from_translocation_rate(
 
 
 #%%
+flux_vs_molar_weight = {}
 data=pd.DataFrame(
     {
     "Probe": ["Fluorescein-Cys", "11 aa peptide", "Insulin", "Aprotinin", 
@@ -300,7 +300,7 @@ data = pd.DataFrame({
     "MM": [630, 100, 68, 29, 29.5, 29.5],
     "Radius": [8.17, 4.13, 3.55, 2.36, 2.51, 2.51],
     "Translocations_": [28, 65, 0.1, 2, 250, 60],# through NPCs at Δc = 1 μM (NPC-1 s-1)
-    "Translocations_empty": [120, 430, 540, 920, 850, 850] #Diffusion rate through a hypothetical ‘plugless’ NPC at Δc = 1 μM (pore−1 s−1)
+    "Translocations_empty": [120, 430, 540, 920, 850, 850] # Diffusion rate through a hypothetical 'plugless' NPC at Δc = 1 μM (pore−1 s−1)
 })
 data["Comment"] = ""
 data.loc[data["Probe"] == 'BSA', "Comment"] = "<0.1"
@@ -316,10 +316,72 @@ Ribbeck2001 = {
 }
 
 flux_vs_molar_weight["Ribbeck2001"] = Ribbeck2001
+#%%
+csv_data="""Protein,Arg/Lys,Charge_pH7.4,Passage_Rate,Mac98A,Nup116,Nsp1,MM,MM_err,Oligomer,Plasmid
+yNTF2 (dimer),10R/6K,-15.0,600.0,290.0,1400.0,800.0,28.0,,1.0,pSF360
+rNTF2 (dimer),4R/8K,-14.6,680.0,3200.0,13000.0,890.0,28.0,,1.0,pDG2121
+mCherry,8R/24K,-6.4,1.0,0.05,0.09,0.4,28.0,,1.0,pSF779
+EGFP,6R/21K,-8.1,3.5,0.11,0.33,0.98,28.0,0.6%,1.0,pSF1526
+shGFP2,11R/24K,-6.2,0.47,0.05,0.05,0.24,29.0,1.4%,1.0,pSF1438
+sinGFP1,1R/34K,-6.4,0.23,0.05,0.05,0.18,28.0,1.5%,1.0,pSF2893
+sinGFP4a,1R/34K,-11.4,0.1,0.05,0.05,,28.0,0.5%,1.0,pDG2754
+efGFP_0W,6R/20K,-10.1,6.0,0.09,0.42,3.0,28.0,1.6%,1.0,pSF2646
+efGFP_3W,6R/20K,-10.1,82.0,1.5,14.0,104.0,33.0,0.5%,1.2,pSF2647
+efGFP_5W,6R/19K,-11.1,87.0,2.2,15.0,170.0,56.0,0.3%,2.0,pSF2648
+efGFP_8F,6R/18K,-11.0,43.0,8.3,51.0,102.0,170.0,1.9%,6.0,pSF2653
+efGFP_8L,6R/18K,-11.0,33.0,1.8,10.0,90.0,59.0,0.2%,2.2,pSF2651
+efGFP_8I,6R/18K,-11.0,68.0,2.9,23.0,102.0,56.0,0.2%,2.1,pSF2650
+efGFP_8M,6R/18K,-11.0,110.0,3.0,17.0,92.0,48.0,0.5%,1.7,pSF2652
+efGFP_8R,14R/18K,-3.0,19.0,0.5,1.9,5.8,30.0,0.7%,1.0,pSF2892
+sffrGFP4,26R/1K,-7.7,160.0,14.0,50.0,9.8,29.0,1.8%,1.0,pDG2805
+sffrGFP4_18xR/K,8R/19K,-8.1,3.8,0.12,0.31,0.76,28.0,2.2%,1.0,pSF2884
+sffrGFP4_25xR/K,1R/26K,-9.2,1.6,0.06,0.1,0.54,27.0,1.8%,1.0,pSF2885
+sffrGFP4_duplicate,26R/1K,-7.7,160.0,14.0,50.0,,29.0,0.3%,1.0,pDG2805
+sffrGFP5,26R/1K,-15.7,64.0,0.67,5.5,,28.0,1.0%,1.0,pDG2806
+sffrGFP6,26R/1K,-1.7,280.0,100.0,160.0,,29.0,1.6%,1.0,pDG2713
+sffrGFP7,26R/1K,-0.3,310.0,200.0,200.0,,29.0,1.4%,1.0,pDG2715
+GFP_MaxR_5W,23R/4K,-7.8,830.0,2100.0,4000.0,690.0,140.0,0.4%,5.0,pSF2903
+GFP_MaxR_8i,25R/1K,-8.7,1300.0,2000.0,4100.0,640.0,47.0,1.6%,1.7,pSF2905
+GFPNTR_2B7,25R/1K,-10.7,1600.0,1200.0,1600.0,,65.0,1.7%,2.4,pDG2718
+GFPNTR_7B3,25R/1K,-9.7,1700.0,1700.0,1400.0,,42.0,2.0%,1.6,pDG2798"""
+
+csv_buffer = StringIO(csv_data)
+
+data = pd.read_csv(csv_buffer)
+_mCherry_influx = 5.7*1e-4
+_nuclear_volume = 1130
+_NPC_number = 2770
+
+data["k"] = data["Passage_Rate"]*_mCherry_influx
+data["tau"] = data["k"]**(-1)
+
+data["R"] = data.apply(
+    lambda _: R_from_k(
+        _["k"], NPC_per_nucleus=_NPC_number,
+        V_nucleus=_nuclear_volume,
+        V_cytoplasm=None,
+        normalize=False
+        ), axis = 1)
+data["Translocations"] = data["R"]**(-1)*6.022e23/1e3
+
+data = data.loc[data["MM"]<35]
+
+Frey2018 = {
+    "data" : data,
+    "Culture" : "HeLa",
+    "NuclearVolume" : _nuclear_volume, #fL
+    "NPCNumber" : _NPC_number,
+    "URL" : "https://doi.org/10.1016/j.cell.2018.05.045",
+    "Reference" : "Frey et al",
+}
+
+flux_vs_molar_weight["Frey2018"] = Frey2018
 # %%
 def calculate_probe_diameter_from_molar_weight(density):
     for k, v in flux_vs_molar_weight.items():
         v["data"]["d"] = v["data"].apply(lambda _: estimate_protein_diameter(_["MM"], density=density), axis = 1)
+    #attractive particles separately
+    Frey2018["data"]["d"]= Frey2018["data"].apply(lambda _: estimate_protein_diameter(_["MM"], density=density), axis = 1)
 
 
 for k, v in flux_vs_molar_weight.items():
@@ -340,6 +402,12 @@ for k, v in flux_vs_molar_weight.items():
     v["data"]["Translocations"] = v["data"]["R"]**(-1)*6.022e23/1e3
 # %%
 if __name__=="__main__":
+    axis_label = {
+        "MM":"MM, [kDa]",
+        "Translocations":"Translocation through NPC\n"+r"at $\Delta c = 1 \mu \text{M}, [\text{s}^{-1}]$",
+        "d":r"$d, [\text{nm}]$",
+        "R":r"$R, [\text{m}^3/\text{s}]$",
+    }
     Kuhn_segment = 0.76
     pore_radius = 26*Kuhn_segment
     L = pore_radius*2
@@ -359,9 +427,17 @@ if __name__=="__main__":
 
     show_text = False
     fig, ax = plt.subplots()
+    ax.set_yscale("log")
+    ax.set_xscale("log")
+    mpl_markers = ('o', '+', 'x', 's', '*')
     markers = itertools.cycle(mpl_markers)
     Y_label = "Translocations"
     X_label = "MM"
+    ax.set_xlabel(axis_label[X_label])
+    ax.set_ylabel(axis_label[Y_label])
+
+    ax.set_xlim(min(empty_pore[X_label]),max(empty_pore[X_label]))
+
     for k, v in flux_vs_molar_weight.items():
         x = v["data"][X_label]
         y = v["data"][Y_label]
@@ -372,13 +448,92 @@ if __name__=="__main__":
                 y=  row[Y_label]
                 s =row["Probe"]
                 ax.text(x,y,s)
-    ax.plot(empty_pore[X_label], empty_pore[Y_label], label = "Empty pore")
+    ax.plot(empty_pore[X_label], empty_pore[Y_label], label = "Empty pore", color = "k")
     ax.legend()
-    #ax.plot(Ribbeck2001["data"]["MM"], Ribbeck2001["data"]["Translocations_empty"])
+    
+    fig.set_size_inches(3.5, 3.5)
+# %%
+    calculate_probe_diameter_from_molar_weight(density)
 
+    empty_pore = {}
+    MM = np.geomspace(1,700)
+    d = estimate_protein_diameter(MM, density)
+    translocations=get_translocation_empty_pore(pore_radius, L, d)
+    #translocations=get_translocation_empty_pore(5, L, d)
+    empty_pore["MM"] = MM
+    empty_pore["d"] = d
+    empty_pore["Translocations"] = translocations
+    empty_pore["R"] = get_R_empty(pore_radius, L, d)
+    #empty_pore["k"] = get_k_empty_pore(pore_radius, L, d, )
+
+    show_text = False
+    fig, ax = plt.subplots()
     ax.set_yscale("log")
     ax.set_xscale("log")
-    ax.set_xlim(1,650)
+    markers = itertools.cycle(mpl_markers)
+    Y_label = "Translocations"
+    X_label = "MM"
+    ax.set_xlabel(axis_label[X_label])
+    ax.set_ylabel(axis_label[Y_label])
 
+    ax.set_xlim(min(empty_pore[X_label]),max(empty_pore[X_label]))
+
+    for k, v in flux_vs_molar_weight.items():
+        x = v["data"][X_label]
+        y = v["data"][Y_label]
+        ax.scatter(x,y, label = v["Reference"], marker = next(markers))
+        if show_text:
+            for idx, row in v["data"].iterrows():
+                x = row[X_label]
+                y=  row[Y_label]
+                s =row["Probe"]
+                ax.text(x,y,s)
+    ax.plot(empty_pore[X_label], empty_pore[Y_label], label = "Empty pore", color = "k")
+    ax.legend()
+    
+    fig.set_size_inches(3.5, 3.5)
+#%%
+    show_text = False
+    fig, ax = plt.subplots()
+    ax.set_yscale("log")
+    ax.set_xscale("log")
+    markers = itertools.cycle(mpl_markers)
+    Y_label = "Translocations"
+    #X_label = "MM"
+    ax.set_xlabel(r"$\left(\frac{c_{\text{in}}}{c_{\text{out}}}\right)_{\text{gel}}$")
+    ax.set_ylabel(axis_label[Y_label])
+    nups = ["Mac98A","Nup116"]
+
+    data = Frey2018["data"].loc[Frey2018["data"]["d"]<5]
+
+    #ax.set_xlim(min(empty_pore[X_label]),max(empty_pore[X_label]))
+
+    y = data[Y_label]
+    mpl_markers = ('*', 'D')
+    markers = itertools.cycle(mpl_markers) 
+    for nup in nups:
+        x = data[nup]
+        ax.scatter(
+            x,y,label = nup,
+            marker = next(markers),
+            )
+    #ax.plot(empty_pore[X_label], empty_pore[Y_label], label = "Empty pore", color = "k")
+    d = 6*Kuhn_segment
+    empty_pore_line = get_translocation_empty_pore(
+            r_p = pore_radius,
+            L=L,
+            d=d,
+            #Frey2018["NPCNumber"],
+            #Frey2018["NuclearVolume"],
+            #Haberman_correction=True
+            )
+    ax.axhline(
+        empty_pore_line, 
+        color =  "black", 
+        linestyle = "-", 
+        label = "empty pore"
+        )
+    ax.legend()
+    
     fig.set_size_inches(3.5, 3.5)
 # %%
