b25c00e15f7a1acee0e04b8cdea390181ba91dd3
ml <miklakt@gmail.com>
2025-06-10 20:50:05 +0200

    stencil

--- solve_poisson.py
+++ solve_poisson.py
@@ -43,7 +43,7 @@ def is_inside_ellipse(x, y, a, b, x0=0.0, y0=0.0, side='full'):
 
 FieldType = np.typing.NDArray[np.float64]
 class PoissonSolver2DCylindrical:
-    def __init__(self, D : FieldType, S : FieldType | typing.Dict[FieldType], BC = None, orientation = "rz", dr = 1, dz = 1):
+    def __init__(self, D : FieldType, S : FieldType, BC = None, orientation = "rz", dr = 1, dz = 1):
         self.D = D
         # if isinstance(S, FieldType):
         #     self.S = {"":S}
@@ -54,6 +54,7 @@ class PoissonSolver2DCylindrical:
         self.N = self.Nr*self.Nz
         self.dr = dr
         self.dz = dz
+        self.b = np.zeros(self.Nr*self.Nz)
     def idx_ij(self, k:int):
         if k>=self.N:
             raise IndexError(f"flatten list index {k} out of range {self.N}")
@@ -87,6 +88,8 @@ class PoissonSolver2DCylindrical:
         else:
             return i, j, self.idx_k(i,j)
     def get_position(self, i:int=None, j:int=None, k:int=None):
+        # returns the left lower corner of the cell
+        # the physical qt are sampled at z+0.5dz r+0.5dr offset
         i, j, k = self.idx(i,j,k)
         r = i*self.dr
         z = j*self.dz
@@ -101,47 +104,110 @@ class PoissonSolver2DCylindrical:
         rp = 2*(r+self.dr)*self.dz
         zm = zp = 2*r*self.dr
         return {"rm":rm, "rp":rp, "zm":zm, "zp":zp}
-    # def get_lambdas(self, i:int=None, j:int=None, k:int=None):
-    #     v =
-        
-    #def get_neigh
-    # def get_cell_type(self, i:int=None, j:int=None, k:int=None):
-    #     i, j, k = self.idx(i,j,k)
-    #     source = self.S
-    #     if source[i,j]: return "source"
-    #     if i==0: return "rm"
-    #     if i==self.Nr-1: return "rp"
-    #def add_terms(self, stencil)
+    def get_lambdas(self, i:int=None, j:int=None, k:int=None):
+        r, z = self.get_position(i, j, k)
+        rm = 2*r/(2*r+1) / self.dr**2
+        rp= (2*r+2*self.dr)/(2*r+1) / self.dr**2
+        zm = zp = 1/self.dz**2
+        return {"rm":rm, "rp":rp, "zm":zm, "zp":zp}
+    
+    def get_D_faces(self, i:int=None, j:int=None, k:int=None):
+        def mean(a,b):
+            if (a==0) or (b==0): return b
+            return (a+b)/2
+        #by default we set Neumann zero-flux conditions
+        if i==0:
+            rm = 0
+        else:
+            rm =  mean(self.D[i - 1, j],self.D[i, j])
+        if i==self.Nr-1:
+            rp = 0
+        else:
+            rp =  mean(self.D[i + 1, j],self.D[i, j])
+        if j == 0:
+            zm = 0
+        else:
+            zm =  mean(self.D[i, j - 1],self.D[i, j])
+        if j == self.Nz-1:
+            zp = 0
+        else:
+            zp =  mean(self.D[i, j + 1],self.D[i, j])
+        return {"rm":rm, "rp":rp, "zm":zm, "zp":zp}
+    
     def get_stencil(self, i:int=None, j:int=None, k:int=None):
         i,j,k = self.idx(i,j,k)
         stencil = {"rm":0, "rp":0, "zm":0, "zp":0, "c":0}
+        l = self.get_lambdas(i,j)
+        D = self.get_D_faces(i,j)
         source = self.S
         source_val = 1.0
+        sink_val = 0.0
 
         r, z = self.get_position(i,j)
         dr = self.dr
         dz = self.dz
 
+        b = self.b
+
+        print(i,j,k)
         if source[i,j]:
+            print("source")
             stencil["c"] = source_val
-        if i==0:
-            Drm =  (self.D[i - 1, j] + self.D[i, j])/2
-            lambda_rm = 2*r/(2*r*dr + dr**2)
-            stencil["rm"]+=Drm * lambda_rm
-            stencil["c"]-=Drm * lambda_rm
+            b[k] = source_val
+        else:
+            # This should not be hard-codded,
+            # but handled by choosing BC
+            # now it is Dirichlet on the outermost face 
+            if j==self.Nz-1:
+                print("last")
+                coef = self.D[i,j]*l["zm"]*2.0
+                stencil["zm"]+=coef
+                stencil["c"]-=coef
+                b[k] = coef*sink_val
+                #block radial flux
+                D["rm"] =  D["rp"] = 0
 
-            Drp =  (self.D[i + 1, j] + self.D[i, j])/2
-            lambda_rm = (2*r+dr)/(2*r*dr + dr**2)
-            stencil["rm"]+=Drp * lambda_rm
-            stencil["c"]-=Drp * lambda_rm
+            coef = D["rm"]*l["rm"]
+            stencil["rm"]+=coef
+            stencil["c"]-=coef
+
+            coef = D["rp"]*l["rp"]
+            stencil["rp"]+=coef
+            stencil["c"]-=coef
+
+            coef = D["zm"]*l["zm"]
+            stencil["zm"]+=coef
+            stencil["c"]-=coef
             
+            coef = D["zp"]*l["zp"]
+            stencil["zp"]+=coef
+            stencil["c"]-=coef
 
-        
-        
+            
+        return stencil
+    
+    def get_stencil_indices(self, i:int=None, j:int=None, k:int=None):
+        i, j, k = self.idx(i,j,k)
+        stencil_indices = {"rm":(i-1,j), "rp":(i-1,j), "zm":(i,j-1), "zp":(i,j+1), "c":(i,j)}
 
     
-
-
-
+    def build_matrix(self):
+        data, rows, cols = [], [], []
+        b = self.b
+        Nr = self.Nr
+        Nz = self.Nz
+        
+        for i in range(Nr):
+            for j in range(Nz):
+                k = self.idx_k(i,j)
+                for neigh, coeff
 
 #%%
+D = np.ones((10,10))
+S = np.zeros_like(D)
+S[:,:3] = True
+
+poisson = PoissonSolver2DCylindrical(D,S)
+# %%
+poisson.get_stencil(2,9)
+# %%
